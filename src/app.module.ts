3fc192e3c7b4bd179803b5ae48f68668ff09a9ac
Dok6n <ehrbs2018@gmail.com>
2022-08-05 18:59:59 +0900

    feat: add service and resolver
    nest cli로 prisma, todos, users의 리졸버와 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { UsersService } from './users/users.service';
+import { TodosService } from './todos/todos.service';
+import { UsersResolver } from './users/users.resolver';
+import { TodosResolver } from './todos/todos.resolver';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UsersService, TodosService, UsersResolver, TodosResolver],
 })
 export class AppModule {}
