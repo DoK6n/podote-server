f0666725dc765a0bf84c2ea16dd9b1b1dcecbe35
Dok6n <ehrbs2018@gmail.com>
2022-08-07 16:32:36 +0900

    chore(seed): run db seed

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Dokyun',
+    email: 'Dok6n@email.io',
+    snsType: {
+      create: {
+        name: 'google',
+      },
+    },
+    todos: {
+      create: [
+        {
+          content: {
+            type: 'doc',
+            content: [
+              {
+                type: 'heading',
+                attrs: { level: 1 },
+                content: [{ type: 'text', text: 'Hello World' }],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
