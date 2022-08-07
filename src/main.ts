e8b7af9fa5994eca0c2ba9bf2a23ddc5ad0e365c
Dok6n <ehrbs2018@gmail.com>
2022-08-07 17:13:42 +0900

    feat: add prisma module

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     Logger.log(`
