ce0370e84984d917e27396092b6852079cb2f43d
Dok6n <ehrbs2018@gmail.com>
2022-08-08 22:49:34 +0900

    feat: add userModule in todoModule

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { TodosResolver } from './todos.resolver';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [UsersModule],
   providers: [TodosService, TodosResolver],
   exports: [TodosService],
 })
