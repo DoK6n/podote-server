2def9d02b54fa18a2c73ba4a007e0d6ffc210187
Dok6n <ehrbs2018@gmail.com>
2022-08-08 22:51:33 +0900

    feat: update todo resolver
    addNewTodo: 할일 추가
    retrieveAllTodos: 유저의 할일 목록 조회
    retrieveTodo: 유저의 특정 할일 단일 조회

--- src/todos/todos.resolver.ts
+++ src/todos/todos.resolver.ts
@@ -1,53 +1,58 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UsersService } from '../users/users.service';
+import { CreateTodoInput } from './dto';
 import { Todo } from './models';
 import { TodosService } from './todos.service';
 
 @Resolver(Todo)
 export class TodosResolver {
-  constructor(private readonly todoService: TodosService) {}
+  constructor(
+    private readonly todoService: TodosService,
+    private readonly userService: UsersService,
+  ) {}
 
-  @Query()
-  async addNewTodo() {
-    return this.todoService.createNewTodo();
+  @Mutation(() => Todo, { nullable: true })
+  async addNewTodo(@Args('data') data: CreateTodoInput) {
+    return this.todoService.createNewTodo(data);
   }
 
-  @Query()
-  async retrieveAllTodos() {
-    return this.todoService.findAllTodosByUser();
+  @Query(() => [Todo], { nullable: true })
+  async retrieveAllTodos(@Args('uid') uid: string) {
+    return this.todoService.findAllTodosByUser(uid);
   }
 
-  @Query()
-  async retrieveTodo() {
-    return this.todoService.findOneTodoById();
+  @Query(() => Todo, { nullable: true })
+  async retrieveTodo(@Args('id') id: string, @Args('uid') uid: string) {
+    return this.todoService.findOneTodoById(id, uid);
   }
 
-  @Query()
-  async retrieveRemovedTodo() {
-    return this.todoService.findOneRemovedTodoByIsRemoved();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async retrieveRemovedTodo() {
+  //   return this.todoService.findOneRemovedTodoByIsRemoved();
+  // }
 
-  @Query()
-  async editTodo() {
-    return this.todoService.updateOneTodoById();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async editTodo() {
+  //   return this.todoService.updateOneTodoById();
+  // }
 
-  @Query()
-  async removeTodo() {
-    return this.todoService.removeOneTodoById();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async removeTodo() {
+  //   return this.todoService.removeOneTodoById();
+  // }
 
-  @Query()
-  async recycleRemovedTodo() {
-    return this.todoService.recycleOneRemovedTodoById();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async recycleRemovedTodo() {
+  //   return this.todoService.recycleOneRemovedTodoById();
+  // }
 
-  @Query()
-  async deleteTodo() {
-    return this.todoService.deleteOneRemoveTodoById();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async deleteTodo() {
+  //   return this.todoService.deleteOneRemoveTodoById();
+  // }
 
-  @Query()
-  async deleteAllRemovedTodos() {
-    return this.todoService.deleteAllRemovedTodos();
-  }
+  // @Query(() => [Todo], { nullable: true })
+  // async deleteAllRemovedTodos() {
+  //   return this.todoService.deleteAllRemovedTodos();
+  // }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,43 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateTodoInput } from './dto';
 
 @Injectable()
 export class TodosService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createNewTodo() {
-    return '';
+  async createNewTodo(data: CreateTodoInput) {
+    const { uid, content } = data;
+    return await this.prisma.todo.create({
+      data: {
+        content: JSON.parse(JSON.stringify(content)),
+        user: {
+          connect: {
+            id: uid,
+          },
+        },
+      },
+    });
   }
 
-  async findAllTodosByUser() {
-    return {};
+  async findAllTodosByUser(uid: string) {
+    return await this.prisma.todo.findMany({
+      where: {
+        userId: uid,
+      },
+    });
   }
 
-  async findOneTodoById() {
-    return {};
+  async findOneTodoById(id: string, uid: string) {
+    return await this.prisma.todo.findFirst({
+      where: {
+        AND: [{ id: id }, { userId: uid }],
+      },
+    });
   }
 
-  async findOneRemovedTodoByIsRemoved() {
-    return {};
-  }
+  // async findOneRemovedTodoByIsRemoved() {
+  //   return {};
+  // }
 
-  async updateOneTodoById() {
-    return {};
-  }
+  // async updateOneTodoById() {
+  //   return {};
+  // }
 
-  async removeOneTodoById() {
-    return {};
-  }
+  // async removeOneTodoById() {
+  //   return {};
+  // }
 
-  async recycleOneRemovedTodoById() {
-    return {};
-  }
+  // async recycleOneRemovedTodoById() {
+  //   return {};
+  // }
 
-  async deleteOneRemoveTodoById() {
-    return {};
-  }
+  // async deleteOneRemoveTodoById() {
+  //   return {};
+  // }
 
-  async deleteAllRemovedTodos() {
-    return {};
-  }
+  // async deleteAllRemovedTodos() {
+  //   return {};
+  // }
 }
