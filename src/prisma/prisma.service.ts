6409be500a73ecefc51d3b7d1ebf7a0a049eeadd
Dok6n <ehrbs2018@gmail.com>
2022-08-12 15:02:53 +0900

    feat: add query duration option in prisma-query-log
    https://github.com/unlight/prisma-query-log/pull/5

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import {
   Logger,
   OnModuleInit,
 } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { createPrismaQueryEventHandler } from 'prisma-query-log';
 
 /**
@@ -31,18 +31,18 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       errorFormat: 'minimal',
     });
 
-    this.$on<any>('query', (event: Prisma.QueryEvent) => {
-      const { duration } = event;
-      const log = createPrismaQueryEventHandler({
+    this.$on<any>(
+      'query',
+      createPrismaQueryEventHandler({
         logger: (query) => {
-          this.logger.verbose(`${query} \u001B[33m+${duration}ms\u001B[0m`);
+          this.logger.verbose(query);
         },
         format: false,
+        queryDuration: true,
         colorQuery: '\u001B[96m',
         colorParameter: '\u001B[90m',
-      });
-      log(event as Prisma.QueryEvent);
-    });
+      }),
+    );
   }
 
   async onModuleInit() {
