0027606fa67a4f892ea998f98867dd50ece4928f
Dok6n <ehrbs2018@gmail.com>
2022-08-21 17:08:02 +0900

    chore: update eslintrc, prettierrc, commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'ci',
+        'ci', // ci구성파일 및 스크립트 변경
         'feat', // 새로운 기능 추가
         'fix', // 버그 수정
         'docs', // 문서 관련
-        'style', // 스타일 변경 (포매팅 수정, 들여쓰기 추가, …)
+        'style', // 코드 의미에 영향을 주지 않는 변경사항 ( white space, formatting, colons )
         'refactor', // 코드 리팩토링
         'test', // 테스트 관련 코드
-        'build', // 빌드 관련 파일 수정
+        'build', // 시스템 또는 외부 종속성에 영향을 미치는 변경사항 (npm, gulp, yarn 레벨)
         'revert', // 다시 롤백
         'perf', // 성능 개선
-        'chore', // 그 외 자잘한 수정
+        'chore', // 그 외 자잘한 수정, 패키지 매니저 설정할 경우, 코드 수정 없이 설정을 변경
       ],
     ],
   },
