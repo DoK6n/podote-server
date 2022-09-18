e5732ee126293da453eb53eba5b27057df0cffc7
Dok6n <ehrbs2018@gmail.com>
2022-09-18 17:11:34 +0900

    ci: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ if [ -z "$cid" ]; then
   echo 'deploy done! ✅'
   sudo docker ps
 else
-  if sudo docker ps --format '{{.Names}}' | grep -Eq "^nodegreen\$"; then
+  if sudo docker ps --format '{{.Names}}' | grep -Eq "^podotegreen\$"; then
     # if current server [green]
     echo '⬆️  ⬆️  ⬆️  ⬆️  blue server up ⬆️  ⬆️  ⬆️  ⬆️ '
     sudo docker-compose -f docker-compose.blue.yml up -d --build
     sleep 0.2
 
     echo '⬇️  ⬇️  ⬇️  ⬇️  green server stop ⬇️  ⬇️  ⬇️  ⬇️ '
-    sudo docker-compose -f docker-compose.green.yml stop nodegreen
+    sudo docker-compose -f docker-compose.green.yml stop podotegreen
     echo 'deploy done! ✅'
     sudo docker ps -a
   else
@@ -29,7 +29,7 @@ else
     sleep 0.2
 
     echo '⬇️  ⬇️  ⬇️  ⬇️  blue server stop ⬇️  ⬇️  ⬇️  ⬇️ '
-    sudo docker-compose -f docker-compose.blue.yml stop nodeblue
+    sudo docker-compose -f docker-compose.blue.yml stop podoteblue
     echo 'deploy done! ✅'
     sudo docker ps -a
   fi
